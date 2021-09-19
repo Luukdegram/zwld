fef0663a9bd2ed4d5761adccb836c6bf881f1be6
Luuk de Gram <luuk@degram.dev>
2021-09-19 16:16:59 +0200

    Declare all 'Linking Metadata Section' objects, as well as parse them

--- src/link.zig
+++ src/link.zig
@@ -123,7 +123,7 @@ pub const LinkMetaData = struct {
         var i: usize = 0;
         while (i < count) : (i += 1) {
             const subsection = subsections.addOneAssumeCapacity();
-            subsection.* = try Subsection.fromReader(reader);
+            subsection.* = try Subsection.fromReader(gpa, reader);
         }
 
         return LinkMetaData{
@@ -139,10 +139,10 @@ pub const LinkMetaData = struct {
 };
 
 pub const Subsection = union(enum) {
-    segment_info,
-    init_funcs,
-    comdat_info,
-    symbol_table,
+    segment_info: []const Segment,
+    init_funcs: []const InitFunc,
+    comdat_info: []const Comdat,
+    symbol_table: []const SymInfo,
 
     pub const Type = enum(u8) {
         WASM_SEGMENT_INFO = 5,
@@ -154,7 +154,83 @@ pub const Subsection = union(enum) {
 
     /// Returns an `Subsection` union that sets the corresponding tag and fields
     /// based on the subsection's type that was found.
-    pub fn fromReader(reader: anytype) @TypeOf(reader)!Subsection {}
+    pub fn fromReader(gpa: *Allocator, reader: anytype) @TypeOf(reader)!Subsection {
+        const sub_type = try leb.readULEB128(u8, reader);
+        const payload_len = try leb.readULEB128(u32, reader);
+        var limited = std.io.limitedReader(reader, payload_len);
+        const limited_reader = limited.reader();
+
+        // every subsection contains a 'count' field
+        const count = try leb.readULEB128(u32, limited_reader);
+
+        switch (@intToEnum(Type, sub_type)) {
+            .segment_info => {
+                var segments = try std.ArrayList(Segment).initCapacity(gpa, count);
+                errdefer segments.deinit();
+
+                var i: usize = 0;
+                while (i < count) : (i += 1) {
+                    const segment = segments.addOneAssumeCapacity();
+                    segment.* = try Segment.fromReader(gpa, limited_reader);
+                }
+                return Subsection{ .segment_info = segments.toOwnedSlice() };
+            },
+            .init_funcs => {
+                var funcs = try std.ArrayList(InitFunc).initCapacity(gpa, count);
+                errdefer funcs.deinit();
+
+                var i: usize = 0;
+                while (i < count) : (i += 1) {
+                    const func = funcs.addOneAssumeCapacity();
+                    func.* = .{
+                        .priority = try leb.readULEB128(u32, limited_reader),
+                        .symbol_index = try leb.readULEB128(u32, limited_reader),
+                    };
+                }
+
+                return Subsection{ .init_funcs = funcs.toOwnedSlice() };
+            },
+            .comdat_info => {
+                var comdats = try std.ArrayList(Comdat).initCapacity(gpa, count);
+                errdefer comdats.deinit();
+
+                var i: usize = 0;
+                while (i < count) : (i += 1) {
+                    const comdat = comdats.addOneAssumeCapacity();
+                    comdat.* = Comdat.fromReader(gpa, limited_reader);
+                }
+                return Subsection{ .comdat_info = comdats.toOwnedSlice() };
+            },
+            .symbol_table => {
+                var symbols = try std.ArrayList(SymInfo).initCapacity(gpa, count);
+                errdefer symbols.deinit();
+
+                var i: usize = 0;
+                while (i < count) : (i += 1) {
+                    const symbol = try symbols.addOneAssumeCapacity();
+                    symbol.* = .{
+                        .kind = @intToEnum(SymInfo.Type, try leb.readULEB128(u8, limited_reader)),
+                        .flags = try leb.readULEB128(u32, limited_reader),
+                    };
+                }
+
+                return Subsection{ .symbol_table = symbols.toOwnedSlice() };
+            },
+        }
+    }
+
+    pub fn deinit(self: *Subsection, gpa: *Allocator) void {
+        switch (self) {
+            .segment_info => |segment_info| for (segment_info) |segment| {
+                gpa.free(segment.name);
+            } else gpa.free(segment_info),
+            .init_funcs => |funcs| gpa.free(funcs),
+            .comdat_info => |comdats| for (comdats) |comdat| {
+                comdat.deinit(gpa);
+            } else gpa.free(comdats),
+            .symbol_table => |table| gpa.free(table),
+        }
+    }
 };
 
 pub const Segment = struct {
@@ -164,6 +240,19 @@ pub const Segment = struct {
     alignment: u32,
     /// Bitfield containing flags for a segment
     flags: u32,
+
+    pub fn fromReader(gpa: *Allocator, reader: anytype) !Segment {
+        const name_len = try leb.readULEB128(u32, reader);
+        const name = gpa.alloc(u8, name_len);
+        errdefer gpa.free(name);
+        try reader.readNoEof(name);
+
+        return Segment{
+            .name = name,
+            .alignment = try leb.readULEB128(u32, reader),
+            .flags = try leb.readULEB128(u32, reader),
+        };
+    }
 };
 
 pub const InitFunc = struct {
@@ -171,4 +260,111 @@ pub const InitFunc = struct {
     priority: u32,
     /// The symbol index of init function (not the function index).
     symbol_index: u32,
+};
+
+pub const Comdat = struct {
+    name: []const u8,
+    /// Must be zero, no flags are currently defined by the tool-convention.
+    flags: u32,
+    symbols: []const ComdatSym,
+
+    pub fn fromReader(gpa: *Allocator, reader: anytype) !Comdat {
+        const name_len = try leb.readULEB128(u32, reader);
+        const name = gpa.alloc(u8, name_len);
+        errdefer gpa.free(name);
+
+        const flags = try leb.readULEB128(u32, reader);
+        if (flags != 0) {
+            return error.UnexpectedValue;
+        }
+
+        const symbol_count = try leb.readULEB128(u32, reader);
+        var symbols = try std.ArrayList(ComdatSym).initCapacity(gpa, symbol_count);
+        errdefer symbols.deinit();
+
+        var i: usize = 0;
+        while (i < symbol_count) : (i += 1) {
+            const symbol = symbols.addOneAssumeCapacity();
+            symbol.* = .{
+                .kind = @intToEnum(ComdatSym.Type, try leb.readULEB128(u8, reader)),
+                .index = try leb.readULEB128(u32, reader),
+            };
+        }
+
+        return Comdat{
+            .name = name,
+            .flags = flags,
+            .symbols = symbols.toOwnedSlice(),
+        };
+    }
+
+    pub fn deinit(self: Comdat, gpa: *Allocator) void {
+        gpa.free(self.name);
+        gpa.free(self.symbols);
+    }
+};
+
+pub const SymInfo = struct {
+    kind: Type,
+    /// Bitfield containings flags for a symbol
+    /// Can contain any of the flags defined in `SymbolFlag`
+    flags: u32,
+
+    pub fn hasFlag(self: SymbolFlag, flag: SymbolFlag) bool {
+        return self.flags & @enumToInt(flag) != 0;
+    }
+
+    pub fn setFlag(self: *SymInfo, flag: SymbolFlag) void {
+        self.flags |= @enumToInt(flag);
+    }
+
+    pub const Type = enum(u8) {
+        SYMTAB_FUNCTION = 0,
+        SYMTAB_DATA = 1,
+        SYMTAB_GLOABL = 2,
+        SYMTAB_SECTION = 3,
+        SYMTAB_EVENT = 4,
+        SYMTAB_TABLE = 5,
+    };
+};
+
+pub const SymbolFlag = enum(u32) {
+    /// Indicates a weak symbol.
+    /// When linking multiple modules defining the same symbol, all weak definitions are discarded
+    /// in favourite of the strong definition. When no strong definition exists, all weak but one definiton is discarded.
+    /// If multiple definitions remain, we get an error: symbol collision.
+    WASM_SYM_BINDING_WEAK = 1,
+    /// Indicates a local, non-exported, non-module-linked symbol.
+    /// The names of local symbols are not required to be unique, unlike non-local symbols.
+    WASM_SYM_BINDING_LOCAL = 2,
+    /// Indicates a hidden symbol. Hidden symbols will not be exported to the link result, but may
+    /// link to other modules.
+    WASM_SYM_VISIBILITY_HIDDEN = 4,
+    /// Indicates an undefined symbol. For non-data symbols, this must match whether the symbol is
+    /// an import or is defined. For data symbols however, determines whether a segment is specified.
+    WASM_SYM_UNDEFINED = 0x10,
+    /// Indicates a symbol of which its intention is to be exported from the wasm module to the host environment.
+    /// This differs from the visibility flag as this flag affects the static linker.
+    WASM_SYM_EXPORTED = 0x20,
+    /// Indicates the symbol uses an explicit symbol name, rather than reusing the name from a wasm import.
+    /// Allows remapping imports from foreign WASM modules into local symbols with a different name.
+    WASM_SYM_EXPLICIT_NAME = 0x40,
+    /// Indicates the symbol is to be included in the linker output, regardless of whether it is used or has any references to it.
+    WASM_SYM_NO_STRIP = 0x80,
+};
+
+pub const ComdatSym = struct {
+    kind: Type,
+    /// Index of the data segment/function/global/event/table within a WASM module.
+    /// The object must not be an import.
+    index: u32,
+
+    pub const Type = enum(u8) {
+        WASM_COMDAT_DATA = 0,
+        WASM_COMDAT_FUNCTION = 1,
+        WASM_COMDAT_GLOBAL = 2,
+        WASM_COMDAT_EVENT = 3,
+        WASM_COMDAT_TABLE = 4,
+        WASM_COMDAT_SECTION = 5,
+    };
 };
